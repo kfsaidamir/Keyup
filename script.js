0106fb32b6311a9710183d0f684f86904782c55e
kf_saidamir <skadirov2009@gmail.com>
2024-04-19 16:57:51 +0500

    keyup

---
+++ script.js
@@ -0,0 +1,29 @@
+const key = document.querySelector('.like');
+const code = document.querySelector('.code');
+const locat = document.querySelector('.location');
+const which = document.querySelector('.which');
+const text = document.querySelector('h1');
+const header = document.querySelector('.header__content');
+const clear = document.querySelector('button')
+
+document.addEventListener('keyup', (e) => {
+    key.style.color = 'white';
+    key.style.fontSize = '25px'
+    which.style.fontSize = '25px'
+    locat.style.fontSize = '25px'
+    code.style.fontSize = '25px'
+    which.style.color = 'white';
+    code.style.color = 'white';
+    locat.style.color = 'white';
+
+    which.innerText = e.which;
+    key.innerText = e.key;
+    code.innerText = e.code;
+    locat.innerText = e.location;
+});
+
+clear.addEventListener('click', () => {
+    location.reload()
+})
+
+
